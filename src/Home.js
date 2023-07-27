1c8cab8aa4ae8c87cd6165be83c207831bd8b4f3
amdsalman <amdsalman04@gmail.com>
2023-07-27 00:14:38 -0400

    WednesdayProgress

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     //use of hooks, helps refresh rendering on screen
 const[blogs, setBlogs] = useState([
     {title:"My New Website", body: "lorem ipsum...", author: "mario", id: 1},
     {title: "Welcome Party", body:'lorem ipsum...', author: "luigi",id: 2},
-    {title: "Web Dev Top Tips", body:'lorem ipsum...', author: "bob",id: 3}
+    {title: "Web Dev Top Tips", body:'lorem ipsum...', author: "mario",id: 3}
 ]);
 
     return (
         <div className="home">
+            <BlogList blogs = {blogs} title = "All blogs"/>
+            <BlogList blogs = {blogs.filter((blog) => blog.author==='mario')} title = "Mario's blogs"/>
 
         </div>
         
