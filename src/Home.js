02908f39ffcc55608341462d579c11412d276f74
Ahmed Salman <amdsalman04@gmail.com>
2023-07-28 06:15:30 -0700

    Tutorial#21_ReactRouts

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,9 @@
-import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    //use of hooks, helps refresh rendering on screen
-const[blogs, setBlogs] = useState([]);//set as empty NOT null, that way it renders initial rendering
-const [isPending, setIsPending] = useState(true);
-const[error, setError] = useState(null);
 
-//useEffect runs on EVERY render unlike useState which runs when ONLY state changes
-//DONT change state inside udseEffect, leads to infinite loop
-useEffect (()=>{
-    setTimeout(() =>{
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(!res.ok){throw Error("could not fetch data.");}
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-            setIsPending (false);//once data is fetched, set to false to stop loading screen
-        }).catch(err => {setError(err.message)})
-    }, 1000);
-}, []);
-
+const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 //----------MAIN RETURN-----------------------------------
     return (
         <div className="home">
