3be06bb0e7de79a37e4ee70bc7e620b618a7933b
amdsalman <amdsalman04@gmail.com>
2023-08-05 12:32:06 -0400

    On vid#29. Learnt states, saving objet to json

--- src/Create.js
+++ src/Create.js
@@ -1,27 +1,63 @@
+import { useState } from "react";
+
 const Create = ()=>{
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog = {title, body, author}
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {'Content-type':"applicaiton/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("new blog added")
+        });
+    
+    }
+
     return(
-            <form>
+        
+<div className="create">
+
+        <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                 type="text"
-                required/>
+                required
+                value = {title} 
+                onChange={(e)=>setTitle(e.target.value)}     
+                />
 
 
                 <label>Blog body:</label>
-               <textarea>
-                required
+               <textarea
+               required
+               value = {body}
+               onChange={(e)=>setBody(e.target.value)}
+               >
                </textarea>
 
                 <label>
-                    <select>
-                        <option value="mario"></option>
-                        <option value="yoshi"></option>
+                    <select
+                    value = {author}
+                    onChange={(e)=>{setAuthor(e.target.value)}}
+                    >
+                        <option value="mario">mario</option>
+                        <option value="yoshi">yoshi</option>
 
                     </select>
                     <button>Add blog</button>
                 </label>
-
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
             </form>
+            </div>
     );
 }
 
